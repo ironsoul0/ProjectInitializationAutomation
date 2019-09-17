466d7edccb4e0f963b5fb1f2b1d3e248a0050a61
ironsoul <timka2609@gmail.com>
2019-09-18 01:06:44 +0600

    Initial commit

--- .my_commands.sh
+++ .my_commands.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 function create() {
-    cd
-    python create.py $1
-    cd /Users/kalle/Documents/Projects/MyProjects/$1
-    git init
-    git remote add origin git@github.com:KalleHallden/$1.git
-    touch README.md
-    git add .
-    git commit -m "Initial commit"
-    git push -u origin master
-    code .
+	cd ~/development/ProjectInitializationAutomation
+	pipenv shell
+	python create.py $1
+	cd ~/development/$1
+	git init
+	git remote add origin git@github.com:ironsoul0/$1.git
+	touch README.md
+	git add .
+	git commit -m "Initial commit"
+	git push -u origin master
+	code .
 }
--- create.py
+++ create.py
@@ -2,17 +2,17 @@ import sys
 import os
 from github import Github
 
-path = "/Users/kalle/Documents/Projects/MyProjects/"
+path = "~/development/"
 
-username = "" #Insert your github username here
-password = "" #Insert your github password here
+username = "ironsoul0" #Insert your github username here
+password = "ohgodwhy7" #Insert your github password here
 
 def create():
-    folderName = str(sys.argv[1])
-    os.makedirs(path + str(folderName))
-    user = Github(username, password).get_user()
-    repo = user.create_repo(folderName)
-    print("Succesfully created repository {}".format(folderName))
+	folderName = str(sys.argv[1])
+	os.makedirs(path + str(folderName))
+	user = Github(username, password).get_user()
+	repo = user.create_repo(folderName)
+	print("Succesfully created repository {}".format(folderName))
 
 if __name__ == "__main__":
-    create()
+	create()
